b8337fae9f539e80d34c1dc0df9e153a1b479143
BubJla <julian@brumbi.de>
2024-06-21 22:42:02 +0200

    Initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,20 @@
+if status is-interactive
+	fastfetch -c ~/.config/fastfetch/presets/config.jsonc
+	alias r 'ranger'
+	alias v 'nvim'
+	alias vim 'nvim'
+	alias c 'clear'
+	alias b 'bluetuith'
+	alias s 'env LD_PRELOAD=/usr/lib/spotify-adblock.so spotify --uri=%U'
+	alias discord 'webcord'
+	alias waterfox 'flatpak run net.waterfox.waterfox'
+	alias github 'flatpak run io.github.shiftey.Desktop'
+	alias spotify 'env LD_PRELOAD=/usr/lib/spotify-adblock.so spotify --uri=%U'
+	alias bluetooth 'bluetuith'
+	alias update 'flatpak update && sudo pacman -Syu && yay -Syu'
+	export EDITOR=nvim
+    # Commands to run in interactive sessions can go here
+end
+set fish_greeting
+set -gx PATH $PATH ~/.local/bin
+
---
+++ systemStart.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+aurInstalled=0
+
+function get {
+	read -p "$1 (y/n) " -n 1 answer
+	echo
+	if [ $answer = y ] || [ $answer = Y ]; then
+		return 1
+	elif [ "$answer" = "n" ] || [ $answer = N ]; then
+		return 0
+	else
+		echo "Invalid input. Please enter 'y' or 'n'."
+		get $1
+	fi
+}
+
+get 'Start install?'
+if [ $? = 0 ]; then
+	exit
+fi
+get 'install yay(AUR)?'
+if [ $? = 1 ]; then
+	sudo pacman -S --noconfirm --needed base-devel git	
+	git clone https://aur.archlinux.org/yay.git
+	cd yay
+	makepkg -si --noconfirm
+	cd ..
+	sudo rm -r yay
+else 
+	echo 'yay has to be installed => exit'
+	exit
+fi
+echo $aurInstalled
+get 'Install basic configuration?(hyprland with waybar, hyprpaper, ... + config files)'
+if [ $? = 1 ]; then
+	sudo pacman -S --noconfirm hyprland waybar hyprpaper alacritty neovim ranger wl-clipboard wofi polkit-gnome
+	get 'copy config files(recommended)?'
+	if [ $? = 1 ]; then
+		cp -r ./.config/alacritty ~/.config
+		cp -r ./.config/fish ~/.config
+		cp -r ./.config/gtk-2.0 ~/.config
+		cp -r ./.config/gtk-3.0 ~/.config
+		cp -r ./.config/hypr ~/.config
+		cp -r ./.config/nvim ~/.config
+		cp -r ./.config/ranger ~/.config
+		cp -r ./.config/waybar ~/.config
+		cp -r ./.config/wofi ~/.config
+	fi
+	get 'Install fish(better shell) + fastfetch(better neofetch)?'
+	if [ $? = 1 ]; then
+		sudo pacman -S --noconfirm fish
+		chsh -s /usr/bin/fish
+		yay -S --noconfirm fastfetch
+		cp -r ./.config/fastfetch ~/.config
+	fi
+
+	get 'Install ly(better greeter)?'
+	if [ $? = 1 ]; then
+		sudo pacman -S --noconfirm ly
+		sudo systemctl enable ly.service
+	fi
+	get 'Install dunst(notifications)?'
+	if [ $? = 1 ]; then
+		sudo pacman -S --noconfirm dunst
+		cp -r ./.config/dunst ~/.config
+	fi
+fi
+get 'Install useful programs(manually decide)'
+if [ $? = 1 ]; then
+	get 'Install waterfox(debloated firefox)?'
+	if [ $? = 1 ]; then
+		get 'Install flatpak(recommended)?'
+		if [ $? = 1 ]; then
+			sudo pacman -S --noconfirm flatpak
+			flatpak install --noninteractive flathub net.waterfox.waterfox	
+		else 
+			yay -S --noconfirm waterfox
+		fi
+	fi
+	get 'Install dolphin(GUI fileManager)?'
+	if [ $? = 1 ]; then
+		sudo pacman -S --noconfirm dolphin
+	fi
+	get 'Install thunderbird(Email client)?'
+	if [ $? = 1 ]; then
+		sudo pacman -S --noconfirm btop
+	fi
+	get 'Install ad-block spotify + cava(beautiful audio visualizer)?'
+	if [ $? = 1 ]; then
+		yay -S --noconfirm spotify-adblock cava-git
+		cp -r ./.config/cava ~/.config
+	fi
+	get 'Install webcord(discord with screen sharing for wayland)?'
+	if [ $? = 1 ]; then
+		yay -S --noconfirm webcord
+	fi
+	get 'Install btop(hardware monitor)?'
+	if [ $? = 1 ]; then
+		sudo pacman -S --noconfirm btop
+	fi
+fi
+get 'reboot now'
+if [ $? = 1 ]; then
+	reboot
+fi
